6fe4cacfd0141dac5f73e9fa1beee4185d1c0959
Dr. Lemon <120197740+TheLemonDr@users.noreply.github.com>
2023-06-27 01:11:12 -0400

    gif not png

--- cloak.js
+++ cloak.js
@@ -12,7 +12,7 @@ function openInNewTab(url) {
 
   var icon = win.document.createElement('link')
   icon.rel = "shortcut icon"
-  icon.href = link + "/Media/Google_Classroom_Logo.png"
+  icon.href = link + "/Media/Google_Classroom_Logo.gif"
   icon.type = "image/png"
   win.document.head.appendChild(icon)
   
