8f1cfc1984e0168908b20852b4cf9cfa9b431a24
Dr. Lemon <120197740+TheLemonDr@users.noreply.github.com>
2023-07-02 05:37:38 -0400

    jj

--- rick.js
+++ rick.js
@@ -1,34 +1,29 @@
+const elCount = document.querySelector('#count');
 let rPressed = false;
 let count = 0;
-
-inputs()
-
-function inputs(){
-    if(rPressed) {
-      count = count + 1;
-    }
-  }
 
 document.body.addEventListener('keydown', keyDown);
 document.body.addEventListener('keyup', keyUp);
 
+function setCount(newCount) {
+  count = newCount;
+  elCount.textContent = newCount;
+}
+
 function keyDown(event) {
   if (event.keyCode == 82) {
-    rPressed = true;
-  }
-}
+    setCount(count + 1);
 
-function keyUp(event) {
-  if (event.keyCode == 82) {
-    rPressed = false;
-  }
-}
+    setTimeout(() => {
+      setCount(0);
+    }, 250);
 
-if (count == 2) {
-    openTab();
+    if (count === 2) {
+      openTab();
+    }
+  }
 }
 
 function openTab() {
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
-    }
-
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+}
