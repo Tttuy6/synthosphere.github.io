7c06560ea1b9b6992e68824c91004768396bd464
Dr. Lemon <120197740+TheLemonDr@users.noreply.github.com>
2023-07-04 06:29:45 -0400

    there

--- rick.js
+++ rick.js
@@ -1,30 +1,32 @@
-const elCount = document.querySelector('#count');
-let rPressed = false;
-let count = 0;
+window.addEventListener('DOMContentLoaded', () => {
+  const elCount = document.querySelector('#count');
+  let rPressed = false;
+  let count = 0;
 
-document.body.addEventListener('keydown', keyDown);
-document.body.addEventListener('keyup', keyUp);
+  document.body.addEventListener('keydown', keyDown);
+  document.body.addEventListener('keyup', keyUp);
 
-function setCount(newCount) {
+  function setCount(newCount) {
   count = newCount;
   elCount.textContent = newCount;
-}
-
-function keyDown(event) {
-  if (event.keyCode == 82) {
-    setCount(count + 1);
-    console.log("R Pressed.");
-
-    setTimeout(() => {
-      setCount(0);
-    }, 250);
-
-    if (count === 2) {
-      openTab();
-    }
   }
-}
 
-function openTab() {
+  function keyDown(event) {
+  if (event.keyCode == 82) {
+      setCount(count + 1);
+      console.log("R Pressed.");
+
+      setTimeout(() => {
+      setCount(0);
+      }, 250);
+
+      if (count === 2) {
+      openTab();
+      }
+  }
+  }
+
+  function openTab() {
   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-}
+  }
+});
