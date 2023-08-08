2e569ac428de173f4dfb5a6e2bde7013d12fbafc
Dr. Lemon <120197740+TheLemonDr@users.noreply.github.com>
2023-08-08 00:02:42 -0400

    fff

---
+++ hotkey/script.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const elCount = document.querySelector('#count');
+    let rPressed = false;
+    let count = 0;
+  
+    document.body.addEventListener('keydown', keyDown);
+    document.body.addEventListener('keyup', keyUp);
+  
+    function setCount(newCount) {
+    count = newCount;
+    elCount.textContent = newCount;
+    }
+  
+    function keyDown(event) {
+    if (event.keyCode == 72) {
+        setCount(count + 1);
+        console.log("H Pressed.");
+  
+        setTimeout(() => {
+        setCount(0);
+        }, 300);
+  
+        if (count === 3) {
+        openTab();
+        }
+    }
+    }
+  
+    function openTab() {
+    window.open('https://classroom.google.com', '_self');
+    }
+  });
