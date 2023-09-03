e680c6549a220c1fa99fad333c1489ecbf926841
Dr. Lemon <120197740+TheLemonDr@users.noreply.github.com>
2023-09-03 01:33:59 -0400

    JUST ONE BOSS

---
+++ g/justOneBoss/js/cards.js
@@ -0,0 +1,1 @@
+document.getElementById("cards").onmousemove=e=>{for(const t of document.getElementsByClassName("card")){const o=t.getBoundingClientRect(),s=e.clientX-o.left,n=e.clientY-o.top;t.style.setProperty("--mouse-x",`${s}px`),t.style.setProperty("--mouse-y",`${n}px`)}};
